753756dba349dc740b9da1ba6b541880596237ff
WRavagoLFT <wendell.ravago@linoflaptech.com>
2024-12-02 15:42:30 +0800

    added transaction for tracking location change

--- src/controller/controllerBatch.ts
+++ src/controller/controllerBatch.ts
@@ -143,46 +143,192 @@ export const deleteBatch = asyncHandler(async (req, res) => {
 
 export const displayBatch = asyncHandler(async (req, res) => {
     const request = new sql.Request(); 
+    const transaction = new sql.Transaction();
 
     const { id } = req.body;
     console.log(req.body, id)
-    
-    request.input("id", sql.Int, id);
-    const query = request.query(`
-        UPDATE Batches 
-        SET LocationStatusId = 2 
-        OUTPUT INSERTED.*
-        WHERE Id = @id
-    `)
-    
+
     try {
-        const batch = await query;
-        res.status(200).json(batch.recordset[0]);
-    } catch (error: any) {
-        console.log(error)
-        res.status(500).send(error.message);
+        await transaction.begin();
+
+        const batchDetails = await transaction.request()
+            .input("id", sql.Int, id)
+            .query(`
+                SELECT Id, ProductId, Quantity, LocationStatusId
+                FROM Batches
+                WHERE Id = @id
+            `)
+        
+        if (batchDetails.recordset.length === 0) {
+            throw new Error("Batch not found");
+        }
+
+        const { Id, ProductId, Quantity, LocationStatusId } = batchDetails.recordset[0];
+
+        const updateBatch = await transaction.request()
+        .input("id", sql.Int, id)
+        .input("NewLocation", sql.Int, 2)
+        .query(`
+            UPDATE Batches
+            SET LocationStatusId = @NewLocation
+            OUTPUT INSERTED.*
+            WHERE Id = @id    
+        `)
+
+        const transferBatch = await transaction.request()
+            .input("Source", sql.Int, LocationStatusId)
+            .input("Destination", sql.Int, 2)
+            .query(`
+                INSERT INTO InternalTransferOrder (SourceLocationId, DestinationLocationId)
+                OUTPUT INSERTED.*
+                VALUES (@Source, @Destination)
+            `)
+
+        const { InternalTransferOrderId } = transferBatch.recordset[0];
+
+        await transaction.request()
+            .input("TransferOrderId", sql.Int, InternalTransferOrderId)
+            .input("ProductId", sql.Int, ProductId)
+            .input("Quantity", sql.Int, Quantity)
+            .input("BatchId", sql.Int, Id)
+            .query(`
+                INSERT InternalTransferOrderDetail (InternalTransferOrderId, ProductId, BatchId, Quantity)
+                VALUES (@TransferOrderId, @ProductId, @BatchId, @Quantity)
+            `)
+
+        await transaction.commit();
+        res.status(200).json({ message: "Batch moved successfully", batch: updateBatch.recordset[0] });
     }
+    catch (error: any) {
+        await transaction.rollback()
+        throw new Error(error.message)
+        console.log(error.message)
+    }
+    
+    // request.input("id", sql.Int, id);
+    // const query = request.query(`
+    //     UPDATE Batches 
+    //     SET LocationStatusId = 2 
+    //     OUTPUT INSERTED.*
+    //     WHERE Id = @id
+    // `)
+
+    // const recordQuery = request.query(`
+    //     SELECT @CurrentLoc = Batches.LocationStatusId
+    //     FROM Batches 
+    //     WHERE Batches.Id = @id
+
+    //     INSERT INTO InternalTransferOrder (SourceLocationId, DestinationLocationId) VALUES
+    //     (@CurrentLoc, 2)
+    //     OUTPUT INSERTED.*     
+    // `)
+
+    // const recordDetailQuery = request.query(`
+    //     SELECT @ProductId  = Batches.ProductId
+    //     FROM Batches
+    //     WHERE Batches.Id = @id
+
+    //     SELECT @BatchId = Batches.Id
+    //     FROM Batches
+    //     WHERE Batches.Id = @id
+
+    //     SELECT @Quantity = Batches.Quantity
+    //     FROM Batches
+    //     WHERE Batches.Id = @id
+
+    //     INSERT INTO InternalTransferOrderDetail (TransferOrderId, ProductId, Quantity) VALUES
+    // `)
+    
+    // try {
+    //     const batch = await query;
+    //     res.status(200).json(batch.recordset[0]);
+    // } catch (error: any) {
+    //     console.log(error)
+    //     res.status(500).send(error.message);
+    // }
 })
 
 export const storeBatch = asyncHandler(async (req, res) => {
+    // const request = new sql.Request(); 
+
+    // const { id } = req.body;
+    // console.log(req.body, id)
+    
+    // request.input("id", sql.Int, id);
+    // const query = request.query(`
+    //     UPDATE Batches 
+    //     SET LocationStatusId = 1 
+    //     OUTPUT INSERTED.*
+    //     WHERE Id = @id
+    // `)
+    
+    // try {
+    //     const batch = await query;
+    //     res.status(200).json(batch.recordset[0]);
+    // } catch (error: any) {
+    //     console.log(error)
+    //     res.status(500).send(error.message);
+    // }
+
     const request = new sql.Request(); 
+    const transaction = new sql.Transaction();
 
     const { id } = req.body;
     console.log(req.body, id)
-    
-    request.input("id", sql.Int, id);
-    const query = request.query(`
-        UPDATE Batches 
-        SET LocationStatusId = 1 
-        OUTPUT INSERTED.*
-        WHERE Id = @id
-    `)
-    
+
     try {
-        const batch = await query;
-        res.status(200).json(batch.recordset[0]);
-    } catch (error: any) {
-        console.log(error)
-        res.status(500).send(error.message);
+        await transaction.begin();
+
+        const batchDetails = await transaction.request()
+            .input("id", sql.Int, id)
+            .query(`
+                SELECT Id, ProductId, Quantity, LocationStatusId
+                FROM Batches
+                WHERE Id = @id
+            `)
+        
+        if (batchDetails.recordset.length === 0) {
+            throw new Error("Batch not found");
+        }
+
+        const { Id, ProductId, Quantity, LocationStatusId } = batchDetails.recordset[0];
+
+        await transaction.request()
+            .input("id", sql.Int, id)
+            .input("NewLocation", sql.Int, 1)
+            .query(`
+                UPDATE Batches
+                SET LocationStatusId = @NewLocation
+                WHERE Id = @id    
+            `)
+
+        const transferBatch = await transaction.request()
+            .input("Source", sql.Int, LocationStatusId)
+            .input("Destination", sql.Int, 1)
+            .query(`
+                INSERT INTO InternalTransferOrder (SourceLocationId, DestinationLocationId)
+                OUTPUT INSERTED.*
+                VALUES (@Source, @Destination)
+            `)
+
+        const { InternalTransferOrderId } = transferBatch.recordset[0];
+
+        await transaction.request()
+            .input("TransferOrderId", sql.Int, InternalTransferOrderId)
+            .input("ProductId", sql.Int, ProductId)
+            .input("Quantity", sql.Int, Quantity)
+            .input("BatchId", sql.Int, Id)
+            .query(`
+                INSERT InternalTransferOrderDetail (InternalTransferOrderId, ProductId, BatchId, Quantity)
+                VALUES (@TransferOrderId, @ProductId, @BatchId, @Quantity)
+            `)
+
+        await transaction.commit();
+        res.status(200).json({ message: "Batch moved successfully" });
+    }
+    catch (error: any) {
+        await transaction.rollback()
+        throw new Error(error.message)
+        console.log(error.message)
     }
 })
